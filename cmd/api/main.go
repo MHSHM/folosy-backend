be2d43d218d07b69531bd0b8aa480cc5afed15e9
Mohamed Hashim <48164891+MHSHM@users.noreply.github.com>
2026-06-21 15:25:05 +0300

    Wire everything up in the main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"folosy-backend/internal/database"
+	"folosy-backend/internal/handler"
+	"folosy-backend/internal/repository"
+	"folosy-backend/internal/service"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("folosy-backend initialized.")
+	dbPool, err := database.Connect(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer dbPool.Close()
+
+	userRepository := repository.NewUserRepository(dbPool)
+	userService := service.NewUserService(userRepository)
+	userHandler := handler.NewUserHandler(userService)
+
+	http.HandleFunc("POST /Register", userHandler.CreateUserHandler)
+
+	log.Println("server listening on :8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
 }
