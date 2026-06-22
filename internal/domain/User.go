274ec99fab340fdd10b1b975d47d26f3dd8f23b7
Mohamed Hashim <48164891+MHSHM@users.noreply.github.com>
2026-06-22 20:28:09 +0300

    Add BudgetStartAt and BudgetEndAt to the user scheme

--- internal/domain/User.go
+++ internal/domain/User.go
@@ -1,17 +1,21 @@
 package domain
 
+import "time"
+
 type User struct {
-	ID           string  `json:"id"`
-	Email        string  `json:"email"`
-	Username     string  `json:"username"`
-	Password     string  `json:"password"`
-	CreatedAt    string  `json:"created_at"`
-	UpdatedAt    string  `json:"updated_at"`
-	TotalBalance float64 `json:"total_balance"`
-	Budget       float64 `json:"budget"`
-	Spent        float64 `json:"spent"`
-	BankID       string  `json:"bank_id"`
-	RefreshToken string  `json:"refresh_token"`
+	ID            string    `json:"id"`
+	Email         string    `json:"email"`
+	Username      string    `json:"username"`
+	Password      string    `json:"password"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+	TotalBalance  float64   `json:"total_balance"`
+	Budget        float64   `json:"budget"`
+	BudgetStartAt time.Time `json:"budget_start_at"`
+	BudgetEndAt   time.Time `json:"budget_end_at"`
+	Spent         float64   `json:"spent"`
+	BankID        string    `json:"bank_id"`
+	RefreshToken  string    `json:"refresh_token"`
 
 	// TODO: Add a list of categories
 	// TODO: Add a list of transactions
